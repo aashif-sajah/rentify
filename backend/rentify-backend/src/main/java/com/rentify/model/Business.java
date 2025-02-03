e21d75d92fe77f4166b9e929e717048b3ebba6de
Aashif Sajah <masaashifsajah@gmail.com>
2025-02-03 19:26:58 +0530

    Added business model

---
+++ backend/rentify-backend/src/main/java/com/rentify/model/Business.java
@@ -0,0 +1,32 @@
+package com.rentify.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Business
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String businessName;
+
+    @Column(nullable = false)
+    private String businessType;
+
+    @Column(unique = true, nullable = false)
+    private String storeSlug; // Unique store URL identifier
+
+    @OneToOne
+    @JoinColumn(name = "owner_id", referencedColumnName = "userId", nullable = false)
+    private Users owner;
+}

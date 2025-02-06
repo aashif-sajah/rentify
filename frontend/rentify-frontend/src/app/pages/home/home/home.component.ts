b86d891811adb5c5402cb2cfc371b686a8ec09b1
Aashif Sajah <masaashifsajah@gmail.com>
2025-02-06 16:36:16 +0530

    Added home page UI

--- frontend/rentify-frontend/src/app/pages/home/home/home.component.ts
+++ frontend/rentify-frontend/src/app/pages/home/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../../shared/footer/footer.component";
+import { HeaderComponent } from "../../../shared/header/header.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FooterComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

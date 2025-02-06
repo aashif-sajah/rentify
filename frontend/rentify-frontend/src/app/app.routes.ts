68ad39315b5e2a5051a6df40af9d6e3129446fac
Aashif Sajah <masaashifsajah@gmail.com>
2025-02-06 16:09:36 +0530

    Updated routes

--- frontend/rentify-frontend/src/app/app.routes.ts
+++ frontend/rentify-frontend/src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { RegisterComponent } from './pages/auth/register/register.component';
 import { BusinessSetupComponent } from './pages/business-setup/business-setup.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { SupportComponent } from './pages/support/support.component';
 
 export const routes: Routes = [
   {
@@ -29,5 +31,18 @@ export const routes: Routes = [
     component: DashboardComponent,
     canActivate: [authGuard],
   },
+  {
+    path:'about-us',
+    component: AboutUsComponent
+
+  },
+  {
+    path:'support',
+    component: SupportComponent
+  },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: '**',
+    redirectTo: '/home',
+  }
 ];

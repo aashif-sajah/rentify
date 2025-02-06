35bac14b493ba437d918585140076d8872ed5a0e
Aashif Sajah <masaashifsajah@gmail.com>
2025-02-06 12:56:45 +0530

    Added register page basic UI

--- frontend/rentify-frontend/src/app/pages/auth/register/register.component.ts
+++ frontend/rentify-frontend/src/app/pages/auth/register/register.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule,RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  
 }

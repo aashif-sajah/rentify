3eece2052979d3616efc22b776c24a0036a03d57
Aashif Sajah <masaashifsajah@gmail.com>
2025-02-10 16:28:00 +0530

    Added Logic to header

--- frontend/rentify-frontend/src/app/shared/header/header.component.ts
+++ frontend/rentify-frontend/src/app/shared/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,4 +10,18 @@ import { RouterLink } from '@angular/router';
 })
 export class HeaderComponent {
 
+  constructor( private authService: AuthService, private router: Router ) { }
+
+
+  isLoggedIn(): boolean
+  {
+    return this.authService.isLoggedIn();
+  }
+
+  logout() {
+    this.authService.clearRole();
+    this.authService.setToken('');
+    this.router.navigate(['/login']);
+  }
+  
 }

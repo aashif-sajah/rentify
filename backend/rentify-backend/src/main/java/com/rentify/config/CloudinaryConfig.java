5a0d6126b38609dea16eaef080632f3133ede7a2
Aashif Sajah <masaashifsajah@gmail.com>
2025-02-13 23:19:34 +0530

    Configured Cloudinary API

--- backend/rentify-backend/src/main/java/com/rentify/config/CloudinaryConfig.java
+++ backend/rentify-backend/src/main/java/com/rentify/config/CloudinaryConfig.java
@@ -1,11 +1,19 @@
 package com.rentify.config;
 
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.cloudinary.*;
 
 @Configuration
-public class CloudinaryConfig
-{
-    @Bean
-
+public class CloudinaryConfig {
+  @Bean
+  public Cloudinary getCloudinary() {
+    return new Cloudinary(
+        ObjectUtils.asMap(
+            "cloud_name", "djafdtwar",
+            "api_key", "669113794948366",
+            "api_secret", "pMoK95h8zN8yKVTbqJOwcdFhc6A",
+            "secure", true));
+  }
 }

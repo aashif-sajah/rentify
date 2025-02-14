b9864a29127a35db77c3a118da6299ea4162cc9a
Aashif Sajah <masaashifsajah@gmail.com>
2025-02-14 23:27:36 +0530

    Implemented Business Creating UI

--- frontend/rentify-frontend/src/app/core/services/business.service.ts
+++ frontend/rentify-frontend/src/app/core/services/business.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BusinessResponse } from '../../models/business-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BusinessService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/api/business/create'; // Adjust API URL if needed
+
+  constructor(private http: HttpClient) {}
+
+  createBusiness(formData: FormData): Observable<BusinessResponse> {
+    return this.http.post<BusinessResponse>(this.apiUrl, formData);
+  }
 }
--- frontend/rentify-frontend/src/app/pages/business-setup/business-setup.component.ts
+++ frontend/rentify-frontend/src/app/pages/business-setup/business-setup.component.ts
@@ -2,6 +2,8 @@ import { NgIf } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BusinessService } from '../../core/services/business.service';
+import { BusinessRequest } from '../../models/business-request';
 
 @Component({
   selector: 'app-business-setup',
@@ -10,6 +12,52 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './business-setup.component.css'
 })
 export class BusinessSetupComponent {
+  businessRequest:BusinessRequest = {
+    businessName: '',
+    businessType: '',
+    description: '',
+    contactEmail: '',
+    phone: '',
+    storeTheme: {
+      fontStyle: '',
+      primaryColor: '',
+      logoUrl: '',
+    }
+  };
+  selectedFile: File | null = null;
 
+  constructor(private businessService: BusinessService) {}
+
+  // Handle file selection
+  onFileSelected(event: any) {
+    if (event.target.files.length > 0) {
+      this.selectedFile = event.target.files[0];
+    }
+  }
+
+  // Submit form data
+  onSubmit() {
+    if (!this.selectedFile) {
+      alert('Please select an image.');
+      return;
+    }
+
+    const formData = new FormData();
+
+    // Convert object to JSON string before appending
+    formData.append('businessRequest', JSON.stringify(this.businessRequest));
+    formData.append('image', this.selectedFile);
+
+    this.businessService.createBusiness(formData).subscribe({
+      next: (response) => {
+        console.log('Business Created:', response);
+        alert('Business created successfully!');
+      },
+      error: (error) => {
+        console.error('Error creating business:', error);
+        alert('Failed to create business.');
+      }
+    })
+  }
 
 }

ce33c07d8a9fb918c1cd2347f2500fc053419d13
Aashif Sajah <masaashifsajah@gmail.com>
2025-02-19 16:33:55 +0530

    Added Product Service

---
+++ frontend/rentify-frontend/src/app/core/services/product-service.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductResponse } from '../../models/product-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductServiceService {
+
+  private apiUrl = 'http://localhost:8080/api/products';
+
+  constructor(private http:HttpClient) { }
+
+  addProduct(formData: FormData):Observable<ProductResponse>{
+    return this.http.post<ProductResponse>(`${this.apiUrl}`, formData);
+  }
+
+  getAllProductsByBusiness(businessId: number):Observable<ProductResponse[]>{
+    return this.http.get<ProductResponse[]>(`${this.apiUrl}/business/${businessId}`);
+  }
+
+  getProductById(productId: number):Observable<ProductResponse>{
+    return this.http.get<ProductResponse>(`${this.apiUrl}/product/${productId}`);
+  }
+}

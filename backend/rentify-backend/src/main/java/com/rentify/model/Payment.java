9e74ea73ba73bff0b95284f78978256f2f89bd3f
Aashif Sajah <masaashifsajah@gmail.com>
2025-03-04 19:50:18 +0530

    Added Payment  enitity

---
+++ backend/rentify-backend/src/main/java/com/rentify/model/Payment.java
@@ -0,0 +1,37 @@
+package com.rentify.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "payments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "booking_id", nullable = false)
+    private Booking booking;  // The related booking
+
+    @Column(nullable = false)
+    private String paymentIntentId;  // Stripe Payment Intent ID
+
+    @Column(nullable = false)
+    private double amountPaid;
+
+    @Column(nullable = false)
+    private String currency;
+
+//    @Enumerated(EnumType.STRING)
+//    private PaymentStatus status;  // SUCCESS, FAILED, PENDING
+
+    private String paymentMethod;
+}
+

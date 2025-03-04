a9ad018c0abff8c2fed5db3281bcbac704d5227a
Aashif Sajah <masaashifsajah@gmail.com>
2025-03-04 20:29:32 +0530

    Added booking Controller

---
+++ backend/rentify-backend/src/main/java/com/rentify/controller/BookingController.java
@@ -0,0 +1,23 @@
+package com.rentify.controller;
+
+import com.rentify.dto.BookingRequest;
+import com.rentify.model.Booking;
+import com.rentify.service.BookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/bookings")
+@RequiredArgsConstructor
+public class BookingController
+{
+    private final BookingService bookingService;
+
+    @PostMapping("/create/{userId}")
+    public ResponseEntity<Booking> createBooking(@RequestBody BookingRequest request, @PathVariable Long userId) {
+        Booking booking = bookingService.createBooking(request, userId);
+        return ResponseEntity.ok(booking);
+    }
+}

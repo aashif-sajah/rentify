d9ac214f6f49f9d9668d7c1256d0bef4c520c61e
ruzaiq <ruzaiqf@gmail.com>
2025-03-11 12:52:03 +0530

    Updated customer view ts

--- frontend/rentify-frontend/src/app/pages/customer-view/customer-view.component.ts
+++ frontend/rentify-frontend/src/app/pages/customer-view/customer-view.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { BusinessResponse } from '../../models/business-response';
+import { ProductResponse } from '../../models/product-response';
+import { AuthService } from '../../core/services/auth.service';
+import { BusinessService } from '../../core/services/business.service';
+import { ProductServiceService } from '../../core/services/product-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer-view',
@@ -6,6 +12,17 @@ import { Component } from '@angular/core';
   templateUrl: './customer-view.component.html',
   styleUrl: './customer-view.component.css'
 })
-export class CustomerViewComponent {
+export class CustomerViewComponent 
+{
+  business!: BusinessResponse;
+  products: ProductResponse[] = [];
 
+  constructor(
+      private authService: AuthService,
+      private businessService: BusinessService,
+      private productService: ProductServiceService,
+      private router: Router
+    ) {}
+
+    
 }

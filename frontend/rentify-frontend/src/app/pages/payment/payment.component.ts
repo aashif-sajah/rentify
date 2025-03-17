bdecf57d36d6e953081009531c59e52ebdbf91e5
Aashif Sajah <masaashifsajah@gmail.com>
2025-03-18 01:27:21 +0530

    added booking logic

--- frontend/rentify-frontend/src/app/pages/payment/payment.component.ts
+++ frontend/rentify-frontend/src/app/pages/payment/payment.component.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
+import { RegisterService } from '../../core/services/register.service';
+import { Router } from '@angular/router';
+import { User } from '../../models/user';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-payment',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './payment.component.html',
-  styleUrl: './payment.component.css'
+  styleUrl: './payment.component.css',
 })
 export class PaymentComponent {
+  user: User = {
+    userFirstName: 'Test',
+    userLastName: 'User',
+    userEmail: '',
+    username: '',
+    userPassword: 'abcd1234',
+    roles: [{ role: 'Customer', roleDescription: 'Customer' }],
+  };
 
+  constructor(
+    private registerService: RegisterService,
+    private router: Router
+  ) {}
+
+  onSubmit() {
+    console.log(this.user);
+    this.registerService.register(this.user).subscribe({
+      next: (response) => {
+        console.log(response, ' Successfully created user');
+        /* Here We Have to Call booking API to Book. */
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+        this.reSetForm();
+      },
+    });
+  }
+
+  reSetForm() {
+    this.user = {
+      userFirstName: 'Test',
+      userLastName: 'User',
+      userEmail: '',
+      username: '',
+      userPassword: 'abcd1234',
+      roles: [{ role: 'Customer', roleDescription: 'Customer' }],
+    };
+  }
 }
